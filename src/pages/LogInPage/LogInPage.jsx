c8ebf56c8fbc037639db16e8f1561400683ea897
Nawelchafi <chafinawel@gmail.com>
2025-04-22 01:26:11 +0200

    fixing conflict

--- src/pages/LogInPage/LogInPage.jsx
+++ src/pages/LogInPage/LogInPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState , useEffect } from 'react';
 import axios from 'axios';
 import "./LogInPage.css"
 function LogInPage() {
@@ -31,6 +31,42 @@ function LogInPage() {
             setError(err.response?.data?.message || "Something went wrong");
         }
     };
+    const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID; //  Google Client ID
+    useEffect(() => {
+
+        window.google?.accounts.id.initialize({ //google object created from the script of gsi used in html 
+            client_id: CLIENT_ID,
+            callback: handleGoogleResponse,
+        });
+
+        window.google?.accounts.id.renderButton(
+            document.getElementById("google-signin-btn"),
+            {
+                theme: "outline",
+                size: "large",
+                text: "continue_with",
+                shape: "pill",
+
+            }
+        );
+    });
+    const handleGoogleResponse = async (response) => {
+        const token = response.credential;
+
+        try {
+            const res = await axios.post("http://localhost:5005/api/auth/google", {
+                token,
+            });
+
+            // Save token if you want
+            localStorage.setItem("authTokenFrom Google", res.data.token);
+            setSuccess("Google signup successful!");
+        } catch (error) {
+            console.error(error);
+            setError("Google signup failed.");
+        }
+    };
+
 
     return (
         <div className="login-container">
@@ -48,6 +84,9 @@ function LogInPage() {
                         <input type="password" placeholder="Password" name="password" value={formData.password} onChange={handleChange} required />
                     </div>
                     <button type="submit" className="login-button">login</button>
+                    <button type="submit" className="google-button" >
+                        <div id="google-signin-btn"></div>
+                    </button>
                 </div>
             </form>
         </div>
